f36c4b3fc5f3aee843847babeed7ccb7f867b761
IvanMakarishchev <denis.flank@gmail.com>
2022-02-08 00:22:18 +0200

    feat: done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let opening = [];
+  let closing = [];
+  let strArray = str.split('');
+  let container = [];
+  bracketsConfig.forEach(el => {
+    el.forEach((el,index) => {
+      if (index%2===0){
+        opening.push(el);
+      } else {
+        closing.push(el);
+      };
+    });
+  });
+  do {
+    if (container.length === 0){
+      if (opening.indexOf(strArray[0])<0){
+        return false;
+      } else {
+        container.push(strArray.shift());
+      }
+    } else {
+      let lastInContainer = container[container.length-1];
+      if (closing.indexOf(strArray[0])>=0){
+        if (closing.indexOf(strArray[0])===opening.indexOf(lastInContainer)){
+          container.pop();
+          strArray.shift();          
+        } else if (opening.indexOf(strArray[0])>=0){
+          container.push(strArray.shift());                 
+        } else {
+          return false;
+        }
+      } else {
+        container.push(strArray.shift());
+      }
+    }
+  } while (strArray.length>0);
+  if (container.length === 0) { return true }
+  else { return false };
 }
